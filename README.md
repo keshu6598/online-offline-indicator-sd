dd68f7990fd9e11fd381f00987e615d97d3ce59b
keshu6598 <kanishkkhandelwal1998@gmail.com>
2023-04-25 23:08:14 +0530

    adding basic snapshot of server

---
+++ README.md
@@ -0,0 +1,1 @@
+Node JS Server for handeling api routes for system design. 
---
+++ index.js
@@ -0,0 +1,30 @@
+// ***************************************************************************
+// Bank API code from Web Dev For Beginners project
+// https://github.com/microsoft/Web-Dev-For-Beginners/tree/main/7-bank-project/api
+// ***************************************************************************
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+// App constants
+const port = process.env.PORT || 3000;
+  
+// Create the Express app & setup middlewares
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.options('*', cors());
+
+
+// Hello World for index page
+app.get('/', function (req, res) {
+    return res.status(200).send("Hello World!");
+})
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+  
